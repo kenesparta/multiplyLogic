37f5dde42a49a3f9bfb20e11030f6398e95ab981
kenesparta <kenesparta@pm.me>
2021-10-17 06:45:01 -0500

    Adding several unit tests

--- multiply_test.go
+++ multiply_test.go
@@ -2,12 +2,14 @@ package multiplyLogic
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"testing"
 )
 
 const coverageAllowed = 0.5
 
+// TestMain Verifies if the coverage percentage is equals to 'coverageAllowed'
 func TestMain(m *testing.M) {
 	rc := m.Run()
 
@@ -23,8 +25,79 @@ func TestMain(m *testing.M) {
 
 // TestMultiply calls TestMultiply with a two float numbers.
 func TestMultiply(t *testing.T) {
-	ans := Multiply(6.0, 7.0)
-	if ans != 42.0 {
-		t.Errorf("The result should be %f", 42.0)
+	tests := []struct {
+		input struct {
+			first  float64
+			second float64
+		}
+		output float64
+	}{
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  0.0,
+				second: 0.0,
+			},
+			output: 0.0,
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  -1.123,
+				second: -125.879,
+			},
+			output: 141.362,
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  102.003,
+				second: -12.358,
+			},
+			output: -1260.553,
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  math.MaxFloat64,
+				second: 0,
+			},
+			output: 0,
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  math.MaxFloat64,
+				second: -1257.127,
+			},
+			output: math.Inf(-1),
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  93651428.108,
+				second: 12577425.127,
+			},
+			output: 1177893825064993.250,
+		},
+	}
+
+	for _, tt := range tests {
+		m := Multiply(tt.input.first, tt.input.second)
+		if Multiply(tt.input.first, tt.input.second) != tt.output {
+			t.Errorf("%.3f is not equal to %.3f", tt.output, m)
+		}
 	}
 }
