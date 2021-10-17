e5ead09e7a57c6fd90af536abe719fe99c185e78
kenesparta <kenesparta@pm.me>
2021-10-17 12:23:35 -0500

    Adding logic to the multiply service

--- multiply.go
+++ multiply.go
@@ -2,9 +2,26 @@ package multiplyLogic
 
 import "math"
 
-const factor = 1000
+const accuracy = 1000
 
-// Multiply The result is a product of two decimal numbers
-func Multiply(p, s float64) float64 {
-	return math.Round(p*s*factor) / factor
+// Factor Main struct to get the data from the user
+type Factor struct {
+	First  *float64 `json:"first_factor"`
+	Second *float64 `json:"second_factor"`
+}
+
+// Product Shows the final result using the accuracy
+func (f *Factor) Product() float64 {
+	return math.Round((*f.First)*(*f.Second)*accuracy) / accuracy
+}
+
+// AreValidNumbers Verifies if the two values are set in the JSON request
+// If one of these values is not set, the result is false
+func (f *Factor) AreValidNumbers() bool {
+	return f.First != nil && f.Second != nil
+}
+
+// IsProductInfinite Validate if the product exceeds the bounds of float64
+func (f *Factor) IsProductInfinite() bool {
+	return f.Product() == math.Inf(0) || f.Product() == math.Inf(-1)
 }
--- multiply_test.go
+++ multiply_test.go
@@ -23,8 +23,8 @@ func TestMain(m *testing.M) {
 	os.Exit(rc)
 }
 
-// TestMultiply calls TestMultiply with a two float numbers.
-func TestMultiply(t *testing.T) {
+// TestMultiplyProduct calls Product with a two float numbers.
+func TestMultiplyProduct(t *testing.T) {
 	tests := []struct {
 		input struct {
 			first  float64
@@ -95,9 +95,110 @@ func TestMultiply(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		m := Multiply(tt.input.first, tt.input.second)
-		if Multiply(tt.input.first, tt.input.second) != tt.output {
-			t.Errorf("%.3f is not equal to %.3f", tt.output, m)
+		f := Factor{
+			First:  &tt.input.first,
+			Second: &tt.input.second,
+		}
+		p := f.Product()
+		if p != tt.output {
+			t.Errorf("%.3f is not equal to %.3f", tt.output, p)
+		}
+	}
+}
+
+// Testing the validation of factors
+func TestMultiplyNotValidFactors(t *testing.T) {
+	var testNumber float64
+	testNumber = 42.42
+	tests := []struct {
+		input struct {
+			first  *float64
+			second *float64
+		}
+	}{
+		{
+			input: struct {
+				first  *float64
+				second *float64
+			}{
+				first:  nil,
+				second: &testNumber,
+			},
+		},
+		{
+			input: struct {
+				first  *float64
+				second *float64
+			}{
+				first:  &testNumber,
+				second: nil,
+			},
+		},
+		{
+			input: struct {
+				first  *float64
+				second *float64
+			}{
+				first:  nil,
+				second: nil,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		f := Factor{
+			First:  tt.input.first,
+			Second: tt.input.second,
+		}
+		if f.AreValidNumbers() {
+			t.Error("The factors of multiplication are valid! Please review the logic, they couldn't be valid.")
+		}
+	}
+}
+
+func TestMultiplyIsProductInfinite(t *testing.T) {
+	tests := []struct {
+		input struct {
+			first  float64
+			second float64
+		}
+	}{
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  math.MaxFloat64,
+				second: 2.5,
+			},
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  -42.42,
+				second: math.MaxFloat64,
+			},
+		},
+		{
+			input: struct {
+				first  float64
+				second float64
+			}{
+				first:  math.MaxFloat64,
+				second: math.MaxFloat64,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		f := Factor{
+			First:  &tt.input.first,
+			Second: &tt.input.second,
+		}
+		if !f.IsProductInfinite() {
+			t.Error("The product should be infinite!")
 		}
 	}
 }
